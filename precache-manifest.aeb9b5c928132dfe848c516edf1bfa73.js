57d2cb5a4c5a066bcaf1f71033866899fddfea3d
Grzegorz Konert <konert.grzegorz@gmail.com>
2019-08-05 21:11:17 +0200

    Updates

---
+++ precache-manifest.aeb9b5c928132dfe848c516edf1bfa73.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d7df3ef9fa277b155df5f4da9109db2",
+    "url": "/OnLineShop/static/media/imgApple.9d7df3ef.jpg"
+  },
+  {
+    "revision": "c9cefae25bd09d73f89c",
+    "url": "/OnLineShop/static/css/main.be8cfb30.chunk.css"
+  },
+  {
+    "revision": "b70de41c4cd785277311",
+    "url": "/OnLineShop/static/js/runtime~main.b70de41c.js"
+  },
+  {
+    "revision": "3e48bfdb6a3418e8b35fefe18919222e",
+    "url": "/OnLineShop/static/media/fontello.3e48bfdb.svg"
+  },
+  {
+    "revision": "9dc921f20f92bb28b44c",
+    "url": "/OnLineShop/static/js/2.9dc921f2.chunk.js"
+  },
+  {
+    "revision": "1e8c4dbc878535ec63e6ec35a7278ec8",
+    "url": "/OnLineShop/static/media/imgPeach.1e8c4dbc.jpg"
+  },
+  {
+    "revision": "ea32eda6793924cd0798fa954e3eaefd",
+    "url": "/OnLineShop/static/media/imgPlum.ea32eda6.jpg"
+  },
+  {
+    "revision": "c9cefae25bd09d73f89c",
+    "url": "/OnLineShop/static/js/main.c9cefae2.chunk.js"
+  },
+  {
+    "revision": "7fabfcc7436636732699caf74aa1b2f9",
+    "url": "/OnLineShop/static/media/imgAnanas.7fabfcc7.jpg"
+  },
+  {
+    "revision": "5b9d1371b400ddf8126bf8319a282512",
+    "url": "/OnLineShop/static/media/fontello.5b9d1371.eot"
+  },
+  {
+    "revision": "2d47d3bcc2294d01bf013bf8b90f555a",
+    "url": "/OnLineShop/static/media/fontello.2d47d3bc.woff2"
+  },
+  {
+    "revision": "07faad2c297ebb78d1f522eac498504b",
+    "url": "/OnLineShop/static/media/fontello.07faad2c.woff"
+  },
+  {
+    "revision": "a6b39ba5a9ed7af3ac125de4f2335e08",
+    "url": "/OnLineShop/static/media/fontello.a6b39ba5.ttf"
+  },
+  {
+    "revision": "9dc921f20f92bb28b44c",
+    "url": "/OnLineShop/static/css/2.2cdc8a9a.chunk.css"
+  },
+  {
+    "revision": "0ad4dd4319315cfd48091cb3334da372",
+    "url": "/OnLineShop/index.html"
+  }
+];
